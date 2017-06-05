e7748737d72e23fc49f60e72884e4332986fbb48
David Ramirez <drgorriz@gmail.com>
2017-06-05 15:46:54 +0200

    test rest service

--- module/Book/test/Service/RestServiceTest.php
+++ module/Book/test/Service/RestServiceTest.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 05/06/2017
- * Time: 15:23
- */
+use Book\Service\RestService;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Zend\Stdlib\ArrayUtils;
+
+class RestServiceTest extends AbstractHttpControllerTestCase{
+
+    protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include __DIR__ . '/../../../../config/application.config.php',
+            include __DIR__ . '/../../config/module.config.php'
+        );
+        parent::setUp();
+
+    }
+
+    /**
+    * @expectedException Book\Service\ResponseError
+    */
+    public function testResponseError()
+    {
+        $restService = new RestService('FakeUser','FakePass','http://www.fakeurl.com');
+        $restService->sendRequest('/noone');
+    }
+
+    public function testResponseValid(){
+        $config = $this->getApplicationServiceLocator()->get('Config');
+        $book_constants = $config['book_constants'];
+
+        $restService = new RestService($book_constants['auth']['username'],$book_constants['auth']['password'],$book_constants['endpoint']['base_url']);
+        $response = $restService->sendRequest($book_constants['endpoint']['routes']);
+        $data = json_decode($response, true);
+        $this->assertArrayHasKey('flightroutes', $data);
+    }
+
+
+}
